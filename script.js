01b6c2c76c23507459b399f22062f8fdbcfc85ff
ketigabunia15-hash <ketigabunia15@gmail.com>
2025-11-14 10:23:37 +0400

    sum and average

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// 5ელემენტიანი მასივი - მაგათი ჯამის და საშულაო არითმეტიკულის გამოთვლა
+const number=[12, 5, 18, 7, 20]
+
+let sum=0;
+for (let i = 0; i < number.length; i++) {
+    sum += number[i];
+}
+
+const average = sum / number.length;
+
+console.log("ჯამი:", sum);
+console.log("საშუალო:", average);
 
